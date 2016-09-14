7af035c9518570e447c30e3a9a43b8a10a4c14a7
Yuyang Liu <yuyang.liu@chaitin.com>
2016-09-15 00:10:30 +0800

    update

---
+++ static/js/189.519ae36cb6384ac1b1e9.js
@@ -0,0 +1,1 @@
+webpackJsonp([189,192],{439:function(n,o){n.exports={rawContent:"\n在debian中安装vbox增强工具，提示无权限。chmod也无法改变，google找到以下解决方案：\n\n```bash\ncd /media/cdrom/\nsudo sh ./VboxLinuxAdditions.run\n```\n\n这好象是loginshell和非loginshell的原因。\n\n",metaData:{layout:"post",title:"vbox安装增强工具提示无权限",excerpt:"在debian下安装vbox增强工具，提示无权限解决办法。",category:"linux",tags:["vbox"],disqus:!0}}}});
